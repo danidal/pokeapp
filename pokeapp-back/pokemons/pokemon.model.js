9a20a88f8d72370f153b97016219bae8d00d8ed4
Daniele <dalmont.daniele@gmail.com>
2019-12-20 15:53:06 +0100

    backend

--- pokeapp-back/pokemons/pokemon.model.js
+++ pokeapp-back/pokemons/pokemon.model.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const pokemonSchema = new Schema({
-    name: { type: String, required: true }
+    name: { type: String, required: true },
+    quantity: { type: Number }
 });
 
 pokemonSchema.set('toJSON');
 
-module.exports = mongoose.model('Pokemon', pokemonSchema);
+module.exports = mongoose.model('Pokemon', pokemonSchema)
--- pokeapp-back/pokemons/pokemon.service.js
+++ pokeapp-back/pokemons/pokemon.service.js
@@ -1,6 +1,7 @@
 const db = require('../_helpers/db')
 const Pokemon = db.Pokemon
-require('mongoose')
+const mongoose = require('mongoose')
+mongoose.set('useFindAndModify', false)
 
 const getPokemon = async name =>
     await Pokemon.find({ name })
@@ -8,11 +9,21 @@ const getPokemon = async name =>
 const getAll = async () =>
     await Pokemon.find()
 
-const addPokemon = async name =>
-    new Pokemon({ name }).save()
+/* const addPokemon = async name =>
+    new Pokemon({ name }).save() */
 
 const deletePokemon = async name =>
     await Pokemon.deleteOne({ name })
+
+const addPokemon = async name =>
+    await Pokemon.findOneAndUpdate(
+        { name },
+        { $inc: { quantity: 1 } },
+        {
+            new: true,
+            upsert: true
+        }
+    )
 
 module.exports = {
     getPokemon,
